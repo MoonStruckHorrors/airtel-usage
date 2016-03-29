59e5c420d97833f17fd1b07d3be50d38629d21e3
Nikhil Vyas <nikhilvyas182@gmail.com>
2016-03-30 00:23:23 +0530

    Initial (and final) Commit.
    Have fun kids.

---
+++ airtel-usage.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import urllib2
+import re
+import os
+import subprocess
+
+def main():
+	#The IP hasn't changed in the last 30 days. Hope it stays the same.
+	r = urllib2.urlopen('http://122.160.230.125:8080/planupdate/');
+	page = r.read();
+	
+	#Quick regex. Will break with a even a minor change of the source website.
+	m = re.search("<p>You are left with only <span>(\d+\.\d+) GB</span> of high-speed data limit.", page);
+	if m != None:
+		#print m.group(1);
+		p = subprocess.Popen("""/usr/bin/notify-send "airtel-usage: You are left with {0} GB this month." """.format(m.group(1)), shell=True);
+		os.waitpid(p.pid, 0);
+	else:
+		p = subprocess.Popen("/usr/bin/notify-send Something went wrong.", shell=True);
+                os.waitpid(p.pid, 0);
+		#print("Something went wrong.");
+		exit();
+
+	
+
+if __name__ == "__main__":
+	main();
