7b8e1894d5cf23ea7660073a17ec7c02465eff76
Nikhil Vyas <nikhilvyas182@gmail.com>
2016-04-17 19:08:43 +0530

    notify-send now works with cron
    Based on the answer: http://askubuntu.com/a/346580

--- airtel-usage.py
+++ airtel-usage.py
@@ -9,20 +9,18 @@ def main():
 	#The IP hasn't changed in the last 30 days. Hope it stays the same.
 	r = urllib2.urlopen('http://122.160.230.125:8080/planupdate/');
 	page = r.read();
-	
+
 	#Quick regex. Will break with a even a minor change of the source website.
 	m = re.search("<p>You are left with only <span>(\d+\.\d+) GB</span> of high-speed data limit.", page);
 	if m != None:
-		#print m.group(1);
-		p = subprocess.Popen("""/usr/bin/notify-send "airtel-usage: You are left with {0} GB this month." """.format(m.group(1)), shell=True);
+		p = subprocess.Popen("""eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)"; /usr/bin/notify-send "airtel-usage: You are left with {0} GB this month." """.format(m.group(1)), shell=True);
 		os.waitpid(p.pid, 0);
 	else:
-		p = subprocess.Popen("/usr/bin/notify-send Something went wrong.", shell=True);
+		p = subprocess.Popen("""eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)"; /usr/bin/notify-send Something went wrong.""", shell=True);
                 os.waitpid(p.pid, 0);
-		#print("Something went wrong.");
 		exit();
 
-	
+
 
 if __name__ == "__main__":
 	main();
